e08b45400bc00502456d3729a925451d1460cd04
Stephen Adhisaputra <stephenadhi@gmail.com>
2023-04-07 15:50:29 +0200

    Checkpoint finished porting - testing needed

--- human_perception/human_perception/human_tracks.py
+++ human_perception/human_perception/human_tracks.py
@@ -1,16 +1,19 @@
 import rclpy
 from rclpy.node import Node
+from rclpy.duration import Duration
+from rclpy.time import Time
 from rclpy.qos import QoSProfile, DurabilityPolicy
+
 from nav_msgs.msg import Odometry, Path
-from geometry_msgs.msg import Pose, PoseStamped
+from geometry_msgs.msg import Pose, PoseStamped, Quaternion
 from visualization_msgs.msg import Marker, MarkerArray
 from zed_interfaces.msg import ObjectsStamped
 from soloco_interfaces.msg import TrackedPerson, TrackedPersons
+
 from tf2_geometry_msgs import do_transform_pose
 from tf2_ros import Buffer, TransformListener
 
 import numpy as np
-import math
 from collections import deque
 
 from robot_track import Robot_track
@@ -23,7 +26,6 @@ class HumanTrackPublisher(Node):
             depth=10,
             reliability=QoSProfile.ReliabilityPolicy.RELIABLE,
             durability=DurabilityPolicy.VOLATILE)
-
         # Declare parameters
         self.declare_parameter('odom_topic', '/locobot/odom')
         self.declare_parameter('detected_obj_topic', '/zed2/zed_node/obj_det/objects')
@@ -33,7 +35,9 @@ class HumanTrackPublisher(Node):
         self.declare_parameter('max_interp_interval', 0.4)
         self.declare_parameter('max_history_length', 12)
         self.declare_parameter('delay_tolerance', 3.0)
-        self.declare_parameter('visualize_bbox', True)
+        self.declare_parameter('visualize_bbox', False) 
+        self.declare_parameter('max_num_agents', 5)
+        self.declare_parameter('track_timeout', 3.0)    
 
         # Get parameter values
         odom_topic = self.get_parameter('odom_topic').get_parameter_value().string_value
@@ -45,14 +49,16 @@ class HumanTrackPublisher(Node):
         self.delay_tolerance = self.get_parameter("delay_tolerance").get_parameter_value().double_value
         self.max_history_length = self.get_parameter("max_history_length").get_parameter_value().integer_value
         self.visualize_bbox = self.get_parameter("visualize_bbox").get_parameter_value().bool_value
+        self.max_num_agents = self.get_parameter('max_num_agents').get_parameter_value().interger_value
+        self.track_timeout = self.get_parameter('track_timeout').get_parameter_value().double_value
         
-        # Create zed detected objects subscriber
+        # Create subscribers
         self.zed_sub = self.create_subscription(ObjectsStamped, detected_obj_topic, self.zed_callback, qos)
-        # Create odom subscriber
-        self.odom_sub = self.create_subscription(Goal, odom_topic, self.odom_callback, qos) 
-        # Create human track publisher
+        self.odom_sub = self.create_subscription(Goal, odom_topic, self.odom_callback, qos)
+        
+        # Create publishers
         self.human_track_interpolated_pub = self.create_publisher(TrackedPersons, human_track_topic, qos)
-        self.marker_pub = self.create_publisher(MarkerArray, 'human_markers', 10)
+        self.marker_array_pub = self.create_publisher(MarkerArray, 'track_markers', 10)
         self.bbox_pub = self.create_publisher(MarkerArray, 'human_bounding_boxes', 10)
 
         # Create tf buffer and transform listener   
@@ -62,161 +68,132 @@ class HumanTrackPublisher(Node):
         # People vector as cache for detected humans
         self.people = TrackedPersons()
         
-        self.person = []
-
     def odom_callback(self, msg):
-        # In an ideal case, where the robot motion and sensors are perfectly accurate and precise, 
-        # the transform between "odom" and "map" frames would be zero. However, in reality, there are always 
-        # errors and uncertainties in robot motion and sensor readings, which can accumulate over time.
-        # This results in drift between the "odom" and "map" frames. Therefore, it is important to use 
-        # localization and mapping algorithms that can account for these errors and correct for drift 
-        # in order to maintain accurate estimates of the robot position and orientation.
-        
         # Get current robot pose in publishing frame (default: 'map')
         robot_in_map_frame = self.pose_transform(msg.pose.pose, self.pub_frame_id, msg.header.frame_id)
-        self.robot_track.interpolated_pose(robot_in_map_frame, self.max_history_length)
-        # Append current robot position to marker array
-        marker_array = MarkerArray()
-        marker_array.markers.append(self.robot_track.marker)
-        # Append people to marker array
-        if len(self.person) != 0:
-            for pers in self.person:
-                track = []
-                for i in range(self.max_history_length + 1):
-                    point = Point()
-                    point.x = person.track.poses[i].position.x
-                    point.y = person.track.poses[i].position.y
-                    
-                    track.append(point)
-                    pers.marker.points = track
-                    marker_array.markers.append(pers.marker)
         
-        self.markerarray_publisher.publish(marker_array.markers)
+        if robot_in_map_frame: # if transform exists, concatenate robot with people and publish
+            self.robot_track.interpolated_pose(robot_in_map_frame, self.max_history_length)
+            # Append current interpolaed robot position to marker array
+            marker_array = MarkerArray()
+            marker_array.markers.append(self.robot_track.marker)
+            # dditionally append people to marker array
+            if len(self.people.tracks) != 0:
+                for person in self.people.tracks:
+                    person_track = []
+                    for i in range(self.max_history_length + 1):
+                        point = Point()
+                        point.x = person.track.poses[i].position.x
+                        point.y = person.track.poses[i].position.y
+                        # Append point to person_track
+                        track.append(point)
+                    # Create new marker for each person track
+                    marker = Marker()
+                    marker.id = person.track_id + 1  # 0 is reserved for the robot
+                    marker.header.frame_id = pub_frame_id
+                    marker.type = self.marker.LINE_STRIP
+                    marker.action = self.marker.ADD
+                    marker.color.a = 1.0
+                    marker.color.r = 0.0
+                    marker.color.g = 0.0
+                    marker.color.b = 1.0
+                    marker.scale.x = 0.05
+                    marker.points = person_track
+                    marker.lifetime = Duration(seconds=0.1)
+                    marker_array.markers.append(marker)
+            # Publish final marker array
+            self.marker_array_pub.publish(marker_array.markers)
 
     def zed_callback(self, msg):
-        # Loop through all detected objects,
-        # Only consider valid tracking
-        if msg.objects[count].tracking_state == 1 and msg.objects[count].label == "Person":
-            for count in range(len(msg.objects)):
+        # Loop through all detected objects, only consider valid tracking
+        if (msg.objects[count].tracking_state == 1 and msg.objects[count].label == "Person"):
+            for obj in range(len(msg.objects)):
+                obj_id = msg.objects[count].label_id
                 # Get the person id and tracking state
                 tracked_person = soloco_interfaces.msg.TrackedPerson()
-                tracked_person.track_id = "Person"
+                tracked_person.track_id = obj_id
                 tracked_person.tracking_state = 1
-                # Position in camera frame
-                person_x_pos = msg.objects[count].position[0]
-                person_y_pos = msg.objects[count].position[1]
-                # Save to current poseStamped message format
-                currPose = geometry_msgs.msg.PoseStamped()
-                currPose.header = msg.header
-                currPose.pose.position.x = person_x_pos
-                currPose.pose.position.y = person_y_pos
-                currPose.pose.position.z = 0.0
-                currPose.pose.orientation.x = 0.0
-                currPose.pose.orientation.y = 0.0
-                currPose.pose.orientation.z = 0.0
-                currPose.pose.orientation.w = 1.0
+                # Position in camera frame, saved in poseStamped message format
+                curr_pose_cam = geometry_msgs.msg.PoseStamped()
+                curr_pose_cam.header = msg.header
+                curr_pose_cam.pose.position.x = msg.objects[count].position[0]
+                curr_pose_cam.pose.position.y = msg.objects[count].position[1]
+                curr_pose_cam.pose.position.z = 0.0
+                curr_pose_cam.pose.orientation = Quaternion(x=0.0, y=0.0, z=0.0, w=1.0)
+                # Convert pose to publishing frame (default: 'map')
+                curr_pose_map = self.pose_transform(msg.pose.pose, self.pub_frame_id, msg.header.frame_id)
+                if curr_pose_map: # if transform exists               
+                    curr_timestamp = self.get_clock().now()
+                    new_object = True
+                    people_count = len(self.people.tracks)
+                    # Check for matching id from previous detections
+                    if len(self.people.tracks) != 0:
+                        for person in self.people.tracks:
+                            if person.track_id == obj.id:
+                                new_object = False
+                                self.add_interpolated_point(curr_pose-map, obj_id, pub_frame_id, curr_timestamp)  
+                    # In case this object does not belong to existing tracks
+                    if new_object:
+                        # Create a new person and append currebt map pose
+                        person = TrackedPerson()
+                        person.current_pose = curr_pose_map
+                        person.track.header.stamp = now()
+                        person.track.poses.append(curr_pose_map)
+            # Delete entries of interpolated points older than 
+            self.prune_old_interpolated_points(timestamp_)
 
-                # First add new points and remove the ones that are too old
-                current_timestamp = self.get_clock().now()
-                self.generate_from_objects(self.objects, current_timestamp)
-        
-                # Delete old entries of interpolated points
-                self.prune_old_interpolated_points(
-                    current_timestamp)  
-
-
-    def transform_array(self, transform):
-        # Create a homogeneous transformation matrix from the transform
-        translation = np.array([transform.transform.translation.x,
-                                transform.transform.translation.y,
-                                transform.transform.translation.z])
-        rotation = np.array([transform.transform.rotation.x,
-                             transform.transform.rotation.y,
-                             transform.transform.rotation.z,
-                             transform.transform.rotation.w])
-        matrix = tf_conversions.transformations.quaternion_matrix(rotation)
-        matrix[:3, 3] = translation
-        return matrix
-
-    def generate_from_objects(self, objects, current_timestamp):
-        for obj in objects.object_list:
-            new_object = True
-            for i in range(len(self.person)):
-                if self.person[i].id == obj.id:
-                    new_object = False
-                    self.person[i].add_interpolated_point(obj, current_timestamp)  
-            # In case this object does not belong to existing tracks
-            if (new_object):
-                # Create a new object of interpolatedTracklets
-                self.person.append(interpolatedTracklet(obj, current_timestamp))
-
-    def prune_old_interpolated_points(self, ts):
-        track_to_delete = []
-        for it in self.person:
-            if ((ts - it.last_timestamp) > (3.)):
-                track_to_delete.append(it)
-                it.marker.action = 2
-
-        for it in track_to_delete:
-            self.person.remove(it)
-
-    def pose_transform(obj, output_frame, input_frame):
-        transformed_obj = PoseStamped()
+    def pose_transform(self, curr_pose, output_frame, input_frame):
+        transformed_pose = PoseStamped()
         try:
             tf = m_tf_buffer.lookup_transform(output_frame, input_frame, self.get_clock().now())
-            transformed_obj = tf2_geometry_msgs.do_transform_pose(obj, tf)
+            transformed_pose = tf2_geometry_msgs.do_transform_pose(curr_pose, tf)
         except tf2_ros.TransformException as ex:
             self.get_logger().error('%s', ex)
+            
+        return transformed_pose if transformed_pose else None
 
-        return transformed_obj if transformed_obj else None
-
-#---------------------------------custom class made by krisna-----------------------------------------------#
-
-class interpolatedTracklet:
-    def __init__(self, obj_, timestamp_, max_history_length=7):
-        self.id = obj_.id
-        self.max_history_length = max_history_length
-        self.odoms = deque(maxlen=90)  # from odom data, not PoseStamped
-        self.points_interp = deque(maxlen=self.max_history_length)
-        self.interp_point = 0.4
-        self.curr_time = 0
-        self.start_time = timestamp_
-        self.arr_interp_padded = np.zeros([self.max_history_length + 1, 4])
-        self.arr_interp_padded[:, -1] = 1.
-
-        self.marker = Marker()
-        self.marker.id = self.id + 1  # 0 is reserved for the robot
-        self.marker.header.frame_id = "locobot/odom"
-        self.marker.type = self.marker.LINE_STRIP
-        self.marker.action = self.marker.ADD
-        self.marker.color.a = 1.0
-        self.marker.color.r = 0.0
-        self.marker.color.g = 0.0
-        self.marker.color.b = 1.0
-        self.marker.scale.x = 0.05
-        self.marker.pose.orientation.x = 0.0
-        self.marker.pose.orientation.y = 0.0
-        self.marker.pose.orientation.z = 0.0
-        self.marker.pose.orientation.w = 1.0
-        self.marker.lifetime = rospy.Duration.from_sec(0.1)
-
-        self.add_interpolated_point(obj_, timestamp_)
-
-    def add_interpolated_point(se
-        num_t_quan_steps = int((self.curr_time - self.odoms[0][2]) / self.interp_point)
+    def add_interpolated_point(self, curr_pose, person_id, pub_frame_id, timestamp_):
+        # get time of current pose
+        det_time = Time(curr_pose.header.stamp)
+        # Create deque object for saving track
+        track = deque(maxlen=90)
+        track.append(curr_pose.poseposition.x, curr_pose.poseposition.y)
+        np_track = np.array(track)
+        # Create padding array to assign zeros for unknown future positions
+        arr_interp_padded = np.zeros([self.max_history_length + 1, 2])
+        # Get number of steps depending on measured pose time and interpolation interval
+        num_t_quan_steps = int((timestamp_ - det_time) / self.max_interp_interval)
         num_t_quan_steps = self.max_history_length if num_t_quan_steps > self.max_history_length else num_t_quan_steps
-        np_odoms = np.array(self.odoms)
-        inter_time_points = self.curr_time - np.arange(num_t_quan_steps + 1) * self.interp_point
-        x_interp = np.interp(inter_time_points, np_odoms[:, 2], np_odoms[:, 0])
-        y_interp = np.interp(inter_time_points, np_odoms[:, 2], np_odoms[:, 1])
-        # arr_interp = np.concatenate([x_interp, y_interp])
-        self.arr_interp_padded[0:num_t_quan_steps + 1, 0] = x_interp
-        self.arr_interp_padded[0:num_t_quan_steps + 1, 1] = y_interp
+        # Get the time points for interpolation
+        inter_time_points = timestamp_ - np.arange(num_t_quan_steps + 1) * self.max_interp_interval
+        # Interpolate using numpy library
+        x_interp = np.interp(inter_time_points, det_time, np_track[:, 0])
+        y_interp = np.interp(inter_time_points, det_time, np_track[:, 1])
+        # Assign the interpolated points to the padded array
+        arr_interp_padded[0:num_t_quan_steps + 1, 0] = x_interp
+        arr_interp_padded[0:num_t_quan_steps + 1, 1] = y_interp    
         if num_t_quan_steps != self.max_history_length:
-            self.arr_interp_padded[num_t_quan_steps + 1:, 0] = x_interp[-1]
-            self.arr_interp_padded[num_t_quan_steps + 1:, 1] = y_interp[-1]
-        self.last_timestamp = timestamp_
+            arr_interp_padded[num_t_quan_steps + 1:, 0] = x_interp[-1]
+            arr_interp_padded[num_t_quan_steps + 1:, 1] = y_interp[-1]
+        # Renew person track
+        self.people.tracks[person_id].track.header.stamp = now()
+        self.people.tracks[person_id].track.poses.clear()
+        for i in range(self.max_history_length + 1):
+            pose_xy = Pose()
+            pose_xy.position.x = arr_interp_padded[i, 0]
+            pose_xy.position.y = arr_interp_padded[i, 1]
+            self.people.tracks[person_id].track.poses.append(pose_xy)    
 
+    def prune_old_interpolated_points(self, timestamp_):
+        track_to_delete = []
+        for person in self.people.tracks:
+            det_time = Time(person.track.header.stamp)
+            if ((timestamp_ - det_time) > self.track_timeout):
+                track_to_delete.append(it)
+        # Delete old tracks
+        for it in track_to_delete:
+            self.people.tracks.remove(person)
+            
 
 def main(args=None):
     rclpy.init(args=args)
@@ -227,12 +204,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
-
-
-
