928093359268d9f46347fdeb84876d0baed8f93c
Stephen Adhisaputra <stephenadhi@gmail.com>
2023-04-13 21:21:04 +0000

    Optimize scan filter from bounding boxes script

--- social_motion_planner/src/laserscan_filter_node.cpp
+++ social_motion_planner/src/laserscan_filter_node.cpp
@@ -1,6 +1,6 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
-#include <zed_interfaces/msg/bounding_box_3d.hpp>
+#include <zed_interfaces/msg/bounding_boxes_3d.hpp>
 #include <zed_interfaces/msg/objects_stamped.hpp>
 #include <algorithm>
 #include <array>
@@ -12,21 +12,19 @@ public:
         : Node("laser_scan_filter") {
         // Declare parameters
         this->declare_parameter<std::string>("detected_obj_topic", "/zed2/zed_node/obj_det/objects");
-        this->declare_parameter<std::string>("map_laserscan_topic", "scan/map");
-        this->declare_parameter<std::string>("filtered_laserscan_topic", "scan/filtered");
+        this->declare_parameter<std::string>("scan_topic", "scan/map");
+        this->declare_parameter<std::string>("filtered_scan_topic", "scan/filtered");
         this->declare_parameter<integer>("max_num_agents", 20);
         this->get_parameter("detected_obj_topic", detected_obj_topic);
-        this->get_parameter("map_laserscan_topic", map_laserscan_topic);
-        this->get_parameter("human_track_topic", human_track_topic);
+        this->get_parameter("scan_topic", laserscan_topic);
         this->get_parameter("filtered_scan_topic", filtered_scan_topic);       
         this->get_parameter("max_num_agents", max_num_agents);
-        
-        float bounding_box_[max_num_agents][8];
-        
+        //Subscribers
         laser_scan_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
-            map_laserscan_topic, 10, std::bind(&LaserScanFilterNode::laserScanCallback, this, std::placeholders::_1));
+            scan_topic, 10, std::bind(&LaserScanFilterNode::laserScanCallback, this, std::placeholders::_1));
         zed_objects_sub_ = this->create_subscription<zed_interfaces::msg::ObjectsStamped>(
             detected_obj_topic, 10, std::bind(&LaserScanFilterNode::zedObjectsCallback, this, std::placeholders::_1));
+        // Publisher
         filtered_scan_pub_ = this->create_publisher<sensor_msgs::msg::LaserScan>(filtered_scan_topic, 10);
     }
 
@@ -34,12 +32,12 @@ private:
     void laserScanCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg){{
         auto filtered_scan = *msg; // make a copy of the original scan
         // Filter the laser scan points that are inside the bounding box
-        for (int i = 0; i <= msg->ranges.size(); i++){
+        for (int i = 0; i < msg->ranges.size(); i++){
             float angle = msg->angle_min + i * msg->angle_increment;
             float x = msg->ranges[i] * cos(angle);
             float y = msg->ranges[i] * sin(angle);
-            for (int k = 0; k <= bounding_box_.size(0); k++) {
-                if (isPointInsideBoundingBox(x, y, bounding_box_[i][k])){
+            if (!bounding_boxes.empty()) {
+                if (isPointInsideBoundingBox(x, y, bounding_boxes_)) {
                     filtered_scan.ranges[i] = std::numeric_limits<float>::quiet_NaN();
                 }
             }
@@ -49,29 +47,38 @@ private:
 
     void zedObjectsCallback(const zed_interfaces::msg::ObjectsStamped::SharedPtr msg){
     // Store the 2D bounding box information for use in filtering the laserscan
-        for (int i = 0; k <= msg->objects.size() || i <= max_num_agents; i++) {
-            // Save bounding box if object is moving
-            if (msg->objects.action_state == 2) {
-                bbox = msg->objects[i].bounding_box_3d;
-                for (int k = 0; k <= 8; k++){
-                    bounding_box_[i][k] = bbox.corners[k];
-                }
-            }
+        int num_agents = std::min(static_cast<int>(msg->objects.size()), max_num_agents);
+        bounding_boxes_.clear();
+        for (int i = 0; i < num_agents; i++) {
+            const auto& corners = msg->objects[i].bounding_boxes_3d.corners;
+            std::array<float, 8> bounding_box;
+            bounding_box.assign(corners.begin(), corners.end());
+            bounding_boxes_.push_back(bounding_box);
+            
         }
     }
     
-    bool isPointInsideBoundingBox(float x, float y, const std::array<float, 8>& bounding_box){
-        // Find the min and max x and y values of the bounding box
-        float min_x = std::min({bounding_box[0], bounding_box[2], bounding_box[4], bounding_box[6]});
-        float max_x = std::max({bounding_box[0], bounding_box[2], bounding_box[4], bounding_box[6]});
-        float min_y = std::min({bounding_box[1], bounding_box[3], bounding_box[5], bounding_box[7]});
-        float max_y = std::max({bounding_box[1], bounding_box[3], bounding_box[5], bounding_box[7]});
-    
-        // Check if the point is inside the bounding box
-        return (x >= min_x && x <= max_x && y >= min_y && y <= max_y);
+    bool isPointInsideBoundingBox(float x, float y, const std::vector<std::array<float, 8>>& bounding_boxes){
+        // Check if the point is inside any of the bounding boxes
+        for (const auto& bounding_box : bounding_boxes) {
+            // Find the min and max x and y values of the bounding box
+            float min_x = std::min({bounding_box[0], bounding_box[2], bounding_box[4], bounding_box[6]});
+            float max_x = std::max({bounding_box[0], bounding_box[2], bounding_box[4], bounding_box[6]});
+            float min_y = std::min({bounding_box[1], bounding_box[3], bounding_box[5], bounding_box[7]});
+            float max_y = std::max({bounding_box[1], bounding_box[3], bounding_box[5], bounding_box[7]});
+
+            // Check if the point is inside the bounding box
+            if (x >= min_x && x <= max_x && y >= min_y && y <= max_y) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
 private:
+    std::vector<std::array<float, 8>> bounding_boxes_ = {};
+
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_sub_;
     rclcpp::Subscription<zed_interfaces::msg::ObjectsStamped>::SharedPtr zed_objects_sub_;
     rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr filtered_scan_pub_;
