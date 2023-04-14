f67d67a59cc3e9abffe0adac04c430b6b965a28a
Stephen Adhisaputra <stephenadhi@gmail.com>
2023-04-14 15:05:15 +0200

    WIP laserscan filter node

--- social_motion_planner/src/laserscan_filter_node.cpp
+++ social_motion_planner/src/laserscan_filter_node.cpp
@@ -1,22 +1,22 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/laser_scan.hpp>
-#include <zed_interfaces/msg/bounding_boxes_3d.hpp>
+#include <zed_interfaces/msg/bounding_box3_d.hpp>
 #include <zed_interfaces/msg/objects_stamped.hpp>
 #include <algorithm>
 #include <array>
 
-class LaserScanFilterNode : public rclcpp::Node
-{
+class LaserScanFilterNode : public rclcpp::Node {
 public:
     LaserScanFilterNode()
         : Node("laser_scan_filter") {
         // Declare parameters
+        std::string human_track_topic, detected_obj_topic, scan_topic, filtered_scan_topic;
         this->declare_parameter<std::string>("detected_obj_topic", "/zed2/zed_node/obj_det/objects");
         this->declare_parameter<std::string>("scan_topic", "scan/map");
         this->declare_parameter<std::string>("filtered_scan_topic", "scan/filtered");
-        this->declare_parameter<integer>("max_num_agents", 20);
+        this->declare_parameter<int64_t>("max_num_agents", 20);
         this->get_parameter("detected_obj_topic", detected_obj_topic);
-        this->get_parameter("scan_topic", laserscan_topic);
+        this->get_parameter("scan_topic", scan_topic);
         this->get_parameter("filtered_scan_topic", filtered_scan_topic);       
         this->get_parameter("max_num_agents", max_num_agents);
         //Subscribers
@@ -29,14 +29,14 @@ public:
     }
 
 private:
-    void laserScanCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg){{
+    void laserScanCallback(const sensor_msgs::msg::LaserScan::SharedPtr msg){
         auto filtered_scan = *msg; // make a copy of the original scan
         // Filter the laser scan points that are inside the bounding box
-        for (int i = 0; i < msg->ranges.size(); i++){
+        for (long unsigned int i = 0; i < msg->ranges.size(); i++){
             float angle = msg->angle_min + i * msg->angle_increment;
             float x = msg->ranges[i] * cos(angle);
             float y = msg->ranges[i] * sin(angle);
-            if (!bounding_boxes.empty()) {
+            if (!bounding_boxes_.empty()) {
                 if (isPointInsideBoundingBox(x, y, bounding_boxes_)) {
                     filtered_scan.ranges[i] = std::numeric_limits<float>::quiet_NaN();
                 }
@@ -50,9 +50,9 @@ private:
         int num_agents = std::min(static_cast<int>(msg->objects.size()), max_num_agents);
         bounding_boxes_.clear();
         for (int i = 0; i < num_agents; i++) {
-            const auto& corners = msg->objects[i].bounding_boxes_3d.corners;
-            std::array<float, 8> bounding_box;
-            bounding_box.assign(corners.begin(), corners.end());
+            const auto& corners = msg->objects[i].bounding_box_3d.corners;
+            std::array  <float, 8> bounding_box;
+            std::copy(std::begin(corners), std::end(corners), std::begin(bounding_box));
             bounding_boxes_.push_back(bounding_box);
             
         }
@@ -77,6 +77,7 @@ private:
     }
 
 private:
+    int max_num_agents = 10;
     std::vector<std::array<float, 8>> bounding_boxes_ = {};
 
     rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_sub_;
