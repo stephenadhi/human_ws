993eaabc216e3065faef3b7ce80bc54713734d67
Stephen Adhisaputra <stephenadhi@gmail.com>
2023-04-18 19:46:02 +0200

    Visualize subgoal using marker point

--- social_motion_planner/src/subgoal_publisher.cpp
+++ social_motion_planner/src/subgoal_publisher.cpp
@@ -1,6 +1,8 @@
 #include <rclcpp/rclcpp.hpp>
 #include <nav_msgs/msg/path.hpp>
+#include <geometry_msgs/msg/point.hpp>
 #include <geometry_msgs/msg/pose_stamped.hpp>
+#include <visualization_msgs/msg/marker.hpp>
 #include <algorithm>
 #include <vector>
 
@@ -9,18 +11,23 @@ public:
     SubGoalPublisher()
         : Node("subgoal_publisher") {
         // Declare parameters
-        std::string human_track_topic, detected_obj_topic, global_plan_topic, subgoal_topic;
+        std::string global_plan_topic, subgoal_topic, subgoal_marker_topic;
         this->declare_parameter<std::string>("global_plan_topic", "plan");
         this->declare_parameter<std::string>("subgoal_topic", "subgoal_pose");
+        this->declare_parameter<std::string>("subgoal_marker_topic", "visualization/subgoal");
         this->declare_parameter<double>("lookahead_dist", 3.0);
+        this->declare_parameter<double>("goal_tolerance", 0.5);
         this->get_parameter("global_plan_topic", global_plan_topic);
         this->get_parameter("subgoal_topic", subgoal_topic);
-        this->get_parameter("lookahead_dist", lookahead_dist);      
+        this->get_parameter("subgoal_marker_topic", subgoal_marker_topic);
+        this->get_parameter("lookahead_dist", lookahead_dist);
+        this->get_parameter("goal_tolerance", goal_tolerance);
         //Subscribers
         global_plan_sub_ = this->create_subscription<nav_msgs::msg::Path>(
             global_plan_topic, 10, std::bind(&SubGoalPublisher::globalPlanCallback, this, std::placeholders::_1));
         // Publisher
         subgoal_pub_ = this->create_publisher<geometry_msgs::msg::PoseStamped>(subgoal_topic, 10);
+        marker_pub_ = this->create_publisher<visualization_msgs::msg::Marker>(subgoal_marker_topic, 10);
     }
 
 private:
@@ -40,13 +47,34 @@ private:
         subgoal_pose.header.stamp = this->get_clock()->now();
 
         subgoal_pub_->publish(subgoal_pose);
+        visualize_subgoal(subgoal_pose);
+    }
 
+    void visualize_subgoal(geometry_msgs::msg::PoseStamped subgoal){
+        visualization_msgs::msg::Marker marker;
+        marker.header.frame_id = "map";
+        marker.header.stamp = this->get_clock()->now();
+        marker.ns = "goals";
+        marker.id = 0;
+        marker.type = visualization_msgs::msg::Marker::CYLINDER;
+        marker.action = visualization_msgs::msg::Marker::ADD;
+        marker.scale.x = goal_tolerance;
+        marker.scale.y = goal_tolerance;
+        marker.scale.z = 0.0;
+        marker.color.a = 0.3;
+        marker.color.r = 1.0;
+        marker.color.g = 1.0;
+        marker.color.b = 0.0;
+        marker.pose.position.x = subgoal.pose.position.x;
+        marker.pose.position.y = subgoal.pose.position.y;
+        marker_pub_->publish(marker);
     }
     
 private:
-double lookahead_dist;
+    double lookahead_dist, goal_tolerance;
     rclcpp::Subscription<nav_msgs::msg::Path>::SharedPtr global_plan_sub_;
     rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr subgoal_pub_;
+    rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_pub_;
 };
 
 int main(int argc, char **argv){
