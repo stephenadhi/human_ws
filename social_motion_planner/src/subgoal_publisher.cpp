98b01e3144b12f5b105243a944616117c6e10551
Stephen Adhisaputra <stephenadhi@gmail.com>
2023-04-18 20:22:50 +0200

    Fix rviz warning

--- social_motion_planner/src/subgoal_publisher.cpp
+++ social_motion_planner/src/subgoal_publisher.cpp
@@ -61,7 +61,6 @@ private:
         marker.scale.x = goal_tolerance;
         marker.scale.y = goal_tolerance;
         marker.scale.z = 0.01;
-        marker.scale.z = 0.0;
         marker.color.a = 0.3;
         marker.color.r = 1.0;
         marker.color.g = 1.0;
