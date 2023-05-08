d44946cc2bac5a6b035a7b05f8d2ea8e8bb9e11e
Stephen Adhisaputra <stephenadhi@gmail.com>
2023-05-08 13:25:33 +0200

    Add bringup launch files

---
+++ soloco_launch/launch/zedbox_bringup.launch.py
@@ -0,0 +1,80 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch_ros.actions import Node
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+
+def generate_launch_description():
+    # Create the launch configuration variables specific to simulation
+    map_file = LaunchConfiguration('map_file')
+    namespace = LaunchConfiguration('namespace')
+    nav2_param_file = LaunchConfiguration('nav2_param_file')
+    robot_model = LaunchConfiguration('robot_model')
+    robot_name = LaunchConfiguration('robot_name')
+
+    # Get the launch directory
+    bringup_dir = get_package_share_directory('soloco_launch')
+    human_perception_dir = get_package_share_directory('human_perception')
+    social_motion_planner_dir = get_package_share_directory('social_motion_planner')
+    interbotix_nav_dir = get_package_share_directory('interbotix_xslocobot_nav')
+
+    default_map_path = os.path.join(bringup_dir, 'maps', 'tb3_house_demo_crowd.yaml')
+    default_nav2_param_path = os.path.join(interbotix_nav_dir, 'config', 'smac_mppi_nav2_params.yaml')
+    
+    declare_robot_model_cmd = DeclareLaunchArgument(
+        'robot_model',
+        default_value='locobot_base',
+        description=('Interbotix LoCoBot model such as `locobot_base` or `locobot_wx250s`.'))
+
+    declare_robot_name_cmd = DeclareLaunchArgument(
+        'robot_name',
+        default_value='locobot',
+        description=('name of the robot (could be anything but defaults to `locobot`).'))
+
+    declare_map_file_cmd = DeclareLaunchArgument(
+        'map_file',
+        default_value=default_map_path)
+
+    declare_nav2_param_file_cmd = DeclareLaunchArgument(
+        'nav2_param_file',
+        default_value=default_nav2_param_path)
+    
+    declare_namespace_cmd = DeclareLaunchArgument(
+        'namespace',
+        default_value='',
+        description='Top-level namespace')
+
+    zed_camera_launch_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(
+          human_perception_dir, 'launch', 'zed2.launch.py')),
+        launch_arguments={
+          'use_rviz': 'false',
+        }.items())
+
+    social_motion_planner_launch_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(
+          social_motion_planner_dir, 'launch', 'social_planner.launch.py')),
+        launch_arguments={
+          'use_rviz': 'false',
+        }.items())
+     
+    # Create the launch description and populate
+    ld = LaunchDescription()
+    # Declare the launch options
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_nav2_param_file_cmd)
+    ld.add_action(declare_robot_model_cmd)
+    ld.add_action(declare_robot_name_cmd)
+    ld.add_action(declare_map_file_cmd)
+
+    # Add the actions to launch all of the nodes
+    ld.add_action(zed_camera_launch_cmd)
+    ld.add_action(social_motion_planner_launch_cmd)
+
+    return ld
