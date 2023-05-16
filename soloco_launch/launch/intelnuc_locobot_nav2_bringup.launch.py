f2304cc3f1e6cbb073d9f2250a6fbc530d78fc4f
Stephen Adhisaputra <stephenadhi@gmail.com>
2023-05-16 15:29:25 +0200

    Separate launch files for neural planner and mppi planner

--- soloco_launch/launch/intelnuc_locobot_nav2_bringup.launch.py
+++ soloco_launch/launch/intelnuc_locobot_nav2_bringup.launch.py
@@ -7,8 +7,13 @@ from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch_ros.actions import Node
 from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
-
+from launch.substitutions import (
+    LaunchConfiguration, 
+    PythonExpression,
+    PathJoinSubstitution,
+    LaunchConfiguration
+)
+from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     # Create the launch configuration variables
@@ -27,7 +32,6 @@ def generate_launch_description():
     interbotix_nav_dir = get_package_share_directory('interbotix_xslocobot_nav')
 
     default_map_path = os.path.join(bringup_dir, 'maps', 'tb3_house_demo_crowd.yaml')
-    default_nav2_param_path = os.path.join(bringup_dir, 'params', 'smac_mppi_nav2_params.yaml')
     
     declare_robot_model_cmd = DeclareLaunchArgument(
         'robot_model',
@@ -51,31 +55,21 @@ def generate_launch_description():
         'map_file',
         default_value=default_map_path)
 
+    declare_nav2_param_filename_cmd = DeclareLaunchArgument(
+        'nav2_param_filename',
+        default_value='smac_mppi_nav2_params.yaml')
+
     declare_nav2_param_file_cmd = DeclareLaunchArgument(
         'nav2_param_file',
-        default_value=default_nav2_param_path)
-    
+        default_value=PathJoinSubstitution([
+            FindPackageShare("soloco_launch"),
+            'params',
+            LaunchConfiguration('nav2_param_filename')]))
+
     declare_namespace_cmd = DeclareLaunchArgument(
         'namespace',
         default_value='',
         description='Top-level namespace')
-    
-    slam_bringup_launch_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(
-          interbotix_nav_dir, 'launch', 'xslocobot_slam_toolbox.launch.py')),
-        launch_arguments={
-          'cmd_vel_topic': '/locobot/commands/velocity',
-          'launch_driver': 'true',
-          # 'map': map_file,
-          'nav2_params_file': nav2_param_file,
-          'robot_model':robot_model,
-          'robot_name':robot_name,
-          'slam_toolbox_mode': 'online_async',
-          'use_camera': 'false', # Not using Intel camera
-          'use_lidar': 'true',
-          'use_rviz': 'false',
-          'use_sim_time': 'false',
-        }.items())
 
     multi_track_visualizer_cmd = Node(
         package='soloco_perception',
@@ -91,6 +85,56 @@ def generate_launch_description():
         output='screen',
         condition=IfCondition(run_human_tf))
 
+    locobot_nav2_bringup_slam_launch_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(
+          interbotix_nav_dir, 'launch', 'xslocobot_slam_toolbox.launch.py')),
+        launch_arguments={
+          'cmd_vel_topic': '/locobot/commands/velocity',
+          'launch_driver': 'true',
+          # 'map': map_file,
+          'nav2_params_file': nav2_param_file,
+          'robot_model':robot_model,
+          'robot_name':robot_name,
+          'slam_toolbox_mode': 'online_async',
+          'use_camera': 'false', # Not using Intel camera
+          'use_lidar': 'true',
+          'use_rviz': 'false',
+          'use_sim_time': 'false',
+        }.items(),
+        condition=IfCondition(PythonExpression(['not ', launch_neural_planner]))
+        )
+    
+    locobot_control_only_launch_cmd = IncludeLaunchDescription(
+        condition=IfCondition(launch_neural_planner),
+        launch_description_source=PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('interbotix_xslocobot_control'),
+                'launch',
+                'xslocobot_control.launch.py',
+            ])
+        ]),
+        launch_arguments={
+            'robot_model':robot_model,
+            'robot_name':robot_name,
+            'use_lidar': 'true',
+            'use_rviz': 'false',
+            'rviz_frame': 'locobot/odom',
+            'use_camera': 'false',
+            'rs_camera_align_depth': 'true',
+            'use_base': 'true',
+            # 'use_dock': 'true',
+            'use_sim_time':'false',
+        }.items(),
+    )
+
+    nav2_planner_server_slam_launch_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(
+          bringup_dir, 'launch', 'nav2_planner_server_slam.launch.py')),
+        launch_arguments={
+          'use_rviz': 'false',
+        }.items(),
+        condition=IfCondition(launch_neural_planner))
+
     soloco_planner_launch_cmd = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(
           soloco_planner_dir, 'launch', 'social_planner.launch.py')),
@@ -98,7 +142,8 @@ def generate_launch_description():
           'use_rviz': 'false',
         }.items(),
         condition=IfCondition(launch_neural_planner))
-     
+
+
     # Create the launch description and populate
     ld = LaunchDescription()
     # Declare the launch options
@@ -106,14 +151,17 @@ def generate_launch_description():
     ld.add_action(declare_run_human_tf_cmd)
     ld.add_action(declare_map_file_cmd)
     ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_nav2_param_filename_cmd)
     ld.add_action(declare_nav2_param_file_cmd)
     ld.add_action(declare_robot_model_cmd)
     ld.add_action(declare_robot_name_cmd)
 
     # Add the actions to launch all of the nodes
-    ld.add_action(slam_bringup_launch_cmd)
     ld.add_action(multi_track_visualizer_cmd)
     ld.add_action(human_tf2_publisher_cmd)
+    ld.add_action(locobot_nav2_bringup_slam_launch_cmd)
+    ld.add_action(locobot_control_only_launch_cmd)
+    ld.add_action(nav2_planner_server_slam_launch_cmd)
     ld.add_action(soloco_planner_launch_cmd)
 
     return ld
