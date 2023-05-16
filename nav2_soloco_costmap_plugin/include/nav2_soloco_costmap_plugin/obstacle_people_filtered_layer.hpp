0cb5d6f29f75e47a1d86d1d8d9c91b8f1d7deb3e
Stephen Adhisaputra <stephenadhi@gmail.com>
2023-05-16 14:00:51 +0200

    Feature: People filter working with obstacle layer through tf with agent footprint clearing

---
+++ nav2_soloco_costmap_plugin/include/nav2_soloco_costmap_plugin/obstacle_people_filtered_layer.hpp
@@ -0,0 +1,296 @@
+/*********************************************************************
+ *
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, 2013, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Willow Garage, Inc. nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *
+ * Author: Eitan Marder-Eppstein
+ *         David V. Lu!!
+ *********************************************************************
+ * Reference tutorial:
+ * https://navigation.ros.org/tutorials/docs/writing_new_costmap2d_plugin.html
+ *********************************************************************/
+// Author: stephenadhi
+
+#ifndef nav2_soloco_costmap_plugin__OBSTACLE_PEOPLE_FILTERED_LAYER_HPP_
+#define nav2_soloco_costmap_plugin__OBSTACLE_PEOPLE_FILTERED_LAYER_HPP_
+
+#include <memory>
+#include <string>
+#include <vector>
+
+#include "rclcpp/rclcpp.hpp"
+#include "laser_geometry/laser_geometry.hpp"
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wreorder"
+#include "tf2_ros/message_filter.h"
+#pragma GCC diagnostic pop
+#include "message_filters/subscriber.h"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "sensor_msgs/msg/point_cloud.hpp"
+#include "sensor_msgs/msg/point_cloud2.hpp"
+#include "nav2_costmap_2d/costmap_layer.hpp"
+#include "nav2_costmap_2d/layered_costmap.hpp"
+#include "nav2_costmap_2d/observation_buffer.hpp"
+#include "nav2_costmap_2d/footprint.hpp"
+
+#include "nav2_soloco_costmap_plugin/geometry/geometry.hpp"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+#include "tf2/transform_datatypes.h"
+#include "tf2/LinearMath/Transform.h"
+#include "tf2_msgs/msg/tf_message.hpp"
+#include "tf2_ros/buffer.h"
+#include "tf2_ros/create_timer_ros.h"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2_ros/transform_listener.h"
+
+namespace nav2_costmap_2d
+{
+
+/**
+ * @class ObstaclePeopleFilteredLayer
+ * @brief Takes in laser and pointcloud data to populate into 2D costmap
+ */
+class ObstaclePeopleFilteredLayer : public CostmapLayer
+{
+public:
+  /**
+   * @brief A constructor
+   */
+  ObstaclePeopleFilteredLayer()
+  {
+    costmap_ = NULL;  // this is the unsigned char* member of parent class Costmap2D.
+  }
+
+  /**
+   * @brief A destructor
+   */
+  virtual ~ObstaclePeopleFilteredLayer();
+  /**
+   * @brief Initialization process of layer on startup
+   */
+  virtual void onInitialize();
+  /**
+   * @brief Update the bounds of the master costmap by this layer's update dimensions
+   * @param robot_x X pose of robot
+   * @param robot_y Y pose of robot
+   * @param robot_yaw Robot orientation
+   * @param min_x X min map coord of the window to update
+   * @param min_y Y min map coord of the window to update
+   * @param max_x X max map coord of the window to update
+   * @param max_y Y max map coord of the window to update
+   */
+  virtual void updateBounds(
+    double robot_x, double robot_y, double robot_yaw, double * min_x,
+    double * min_y,
+    double * max_x,
+    double * max_y);
+  /**
+   * @brief Update the costs in the master costmap in the window
+   * @param master_grid The master costmap grid to update
+   * @param min_x X min map coord of the window to update
+   * @param min_y Y min map coord of the window to update
+   * @param max_x X max map coord of the window to update
+   * @param max_y Y max map coord of the window to update
+   */
+  virtual void updateCosts(
+    nav2_costmap_2d::Costmap2D & master_grid,
+    int min_i, int min_j, int max_i, int max_j);
+
+  /**
+   * @brief Deactivate the layer
+   */
+  virtual void deactivate();
+
+  /**
+   * @brief Activate the layer
+   */
+  virtual void activate();
+
+  /**
+   * @brief Reset this costmap
+   */
+  virtual void reset();
+
+  /**
+   * @brief If clearing operations should be processed on this layer or not
+   */
+  virtual bool isClearable() {return true;}
+
+  /**
+   * @brief  A method to get the framenames of the system know how many agents there are.
+   */
+  void getFrameNames();
+
+  /**
+   * @brief Callback executed when a parameter change is detected
+   * @param event ParameterEvent message
+   */
+  rcl_interfaces::msg::SetParametersResult
+  dynamicParametersCallback(std::vector<rclcpp::Parameter> parameters);
+
+  /**
+   * @brief triggers the update of observations buffer
+   */
+  void resetBuffersLastUpdated();
+
+  /**
+   * @brief  A callback to handle buffering LaserScan messages
+   * @param message The message returned from a message notifier
+   * @param buffer A pointer to the observation buffer to update
+   */
+  void laserScanCallback(
+    sensor_msgs::msg::LaserScan::ConstSharedPtr message,
+    const std::shared_ptr<nav2_costmap_2d::ObservationBuffer> & buffer);
+
+  /**
+   * @brief A callback to handle buffering LaserScan messages which need filtering to turn Inf values into range_max.
+   * @param message The message returned from a message notifier
+   * @param buffer A pointer to the observation buffer to update
+   */
+  void laserScanValidInfCallback(
+    sensor_msgs::msg::LaserScan::ConstSharedPtr message,
+    const std::shared_ptr<nav2_costmap_2d::ObservationBuffer> & buffer);
+
+  /**
+   * @brief  A callback to handle buffering PointCloud2 messages
+   * @param message The message returned from a message notifier
+   * @param buffer A pointer to the observation buffer to update
+   */
+  void pointCloud2Callback(
+    sensor_msgs::msg::PointCloud2::ConstSharedPtr message,
+    const std::shared_ptr<nav2_costmap_2d::ObservationBuffer> & buffer);
+
+  // for testing purposes
+  void addStaticObservation(nav2_costmap_2d::Observation & obs, bool marking, bool clearing);
+  void clearStaticObservations(bool marking, bool clearing);
+
+protected:
+  /**
+   * @brief  Get the observations used to mark space
+   * @param marking_observations A reference to a vector that will be populated with the observations
+   * @return True if all the observation buffers are current, false otherwise
+   */
+  bool getMarkingObservations(
+    std::vector<nav2_costmap_2d::Observation> & marking_observations) const;
+
+  /**
+   * @brief  Get the observations used to clear space
+   * @param clearing_observations A reference to a vector that will be populated with the observations
+   * @return True if all the observation buffers are current, false otherwise
+   */
+  bool getClearingObservations(
+    std::vector<nav2_costmap_2d::Observation> & clearing_observations) const;
+
+  /**
+   * @brief  Clear freespace based on one observation
+   * @param clearing_observation The observation used to raytrace
+   * @param min_x
+   * @param min_y
+   * @param max_x
+   * @param max_y
+   */
+  virtual void raytraceFreespace(
+    const nav2_costmap_2d::Observation & clearing_observation,
+    double * min_x, double * min_y,
+    double * max_x,
+    double * max_y);
+
+  /**
+   * @brief Process update costmap with raytracing the window bounds
+   */
+  void updateRaytraceBounds(
+    double ox, double oy, double wx, double wy, double max_range, double min_range,
+    double * min_x, double * min_y,
+    double * max_x,
+    double * max_y);
+
+  std::vector<geometry_msgs::msg::Point> transformed_footprint_;
+  bool footprint_clearing_enabled_;
+  /**
+   * @brief Clear costmap layer info below the robot's footprint
+   */
+  void updateFootprint(
+    double robot_x, double robot_y, double robot_yaw, double * min_x,
+    double * min_y,
+    double * max_x,
+    double * max_y);
+
+  std::string global_frame_;  ///< @brief The global frame for the costmap
+  double min_obstacle_height_;  ///< @brief Max Obstacle Height
+  double max_obstacle_height_;  ///< @brief Max Obstacle Height
+
+  /// @brief Used to project laser scans into point clouds
+  laser_geometry::LaserProjection projector_;
+  /// @brief Used for the observation message filters
+  std::vector<std::shared_ptr<message_filters::SubscriberBase<rclcpp_lifecycle::LifecycleNode>>>
+  observation_subscribers_;
+  /// @brief Used to make sure that transforms are available for each sensor
+  std::vector<std::shared_ptr<tf2_ros::MessageFilterBase>> observation_notifiers_;
+  /// @brief Used to store observations from various sensors
+  std::vector<std::shared_ptr<nav2_costmap_2d::ObservationBuffer>> observation_buffers_;
+  /// @brief Used to store observation buffers used for marking obstacles
+  std::vector<std::shared_ptr<nav2_costmap_2d::ObservationBuffer>> marking_buffers_;
+  /// @brief Used to store observation buffers used for clearing obstacles
+  std::vector<std::shared_ptr<nav2_costmap_2d::ObservationBuffer>> clearing_buffers_;
+
+  /// @brief Dynamic parameters handler
+  rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr dyn_params_handler_;
+
+  // Used only for testing purposes
+  std::vector<nav2_costmap_2d::Observation> static_clearing_observations_;
+  std::vector<nav2_costmap_2d::Observation> static_marking_observations_;
+
+  bool rolling_window_;
+  bool was_reset_;
+  int combination_method_;
+
+  bool people_filtering_enabled_;
+  std::string tf_prefix_;
+  float filter_radius_;
+  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
+  std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
+
+  std::vector<std::string> agent_ids_;
+  rclcpp::Subscription<tf2_msgs::msg::TFMessage>::SharedPtr tf_sub_;
+
+  void doTouch(
+    tf2::Transform agent, double * min_x, double * min_y,
+    double * max_x, double * max_y);
+  bool getAgentTFs(std::vector<tf2::Transform> & agents) const;
+  void agentFilter(tf2::Transform agent, float r);
+
+};
+
+}  // namespace nav2_costmap_2d
+
+#endif  // nav2_soloco_costmap_plugin__OBSTACLE_PEOPLE_FILTERED_LAYER_HPP_
