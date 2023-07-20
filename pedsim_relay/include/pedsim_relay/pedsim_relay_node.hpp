7b55e73af11e9646febe8fb162817b015757c4d7
Stephen Adhisaputra <stephenadhi@gmail.com>
2023-07-20 16:09:34 +0000

    Feature: check if agent is in FOV and costmap

--- pedsim_relay/include/pedsim_relay/pedsim_relay_node.hpp
+++ pedsim_relay/include/pedsim_relay/pedsim_relay_node.hpp
@@ -20,9 +20,12 @@
 
 #include <algorithm>
 #include <math.h>
+#include <angles/angles.h>
 
 #include "rclcpp/rclcpp.hpp"
 
+#include <geometry_msgs/msg/pose.hpp>
+#include <nav_msgs/msg/odometry.hpp>
 #include <nav_msgs/msg/occupancy_grid.hpp>
 #include <pedsim_msgs/msg/agent_states.hpp>
 #include <soloco_interfaces/msg/tracked_agent.hpp>
@@ -41,13 +44,18 @@ private:
   double map_origin_x = 0.0;
   double map_origin_y = 0.0;
   double map_resolution = 0.1;
-
+  
+  double field_of_view_ = 2.0944;
+  double robot_theta_ = 0.0;
+  geometry_msgs::msg::Pose robot_pose_;
   std::string pub_frame_id_;
 
+  void odomCallback(const nav_msgs::msg::Odometry::SharedPtr msg);
   void agentsCallback(const pedsim_msgs::msg::AgentStates::SharedPtr msg);
   void costmapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg);
-  bool check_agent_in_local_costmap(double agent_x, double agent_y);
-  
+  bool check_agent_in_fov_and_costmap(double agent_x, double agent_y);
+
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_;
   rclcpp::Subscription<pedsim_msgs::msg::AgentStates>::SharedPtr pedsim_sub_;
   rclcpp::Subscription<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_sub_;
   rclcpp::Publisher<soloco_interfaces::msg::TrackedAgents>::SharedPtr det_agents_pub_;
--- pedsim_relay/src/pedsim_relay_node.cpp
+++ pedsim_relay/src/pedsim_relay_node.cpp
@@ -21,23 +21,44 @@ using namespace geometry_msgs::msg;
 
 namespace pedsim {
 PedsimRelayNode::PedsimRelayNode() : Node("pedsim_relay_node") {
-  std::string local_costmap_topic, detected_agents_topic;
+  std::string costmap_topic, detected_agents_topic, odom_topic;
+  this->declare_parameter<std::string>("odom_topic", "locobot/odom");
   this->declare_parameter<std::string>("pub_frame_id", "locobot/odom");
-  this->declare_parameter<std::string>("local_costmap_topic", "local_costmap/costmap"); 
+  this->declare_parameter<std::string>("costmap_topic", "global_costmap/costmap"); 
   this->declare_parameter<std::string>("detected_agents_topic", "human/simulated_agents");
+  this->declare_parameter<double>("field_of_view", 2.0944);
+  this->get_parameter("odom_topic", odom_topic);
   this->get_parameter("pub_frame_id", pub_frame_id_);
-  this->get_parameter("local_costmap_topic", local_costmap_topic);
+  this->get_parameter("costmap_topic", costmap_topic);
   this->get_parameter("detected_agents_topic", detected_agents_topic);
+  this->get_parameter("field_of_view", field_of_view_);
+  // Subscribe to robot odometry
+  odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+        odom_topic, 10, std::bind(&PedsimRelayNode::odomCallback, this, std::placeholders::_1));
   // Subscribe to simulated agents
   pedsim_sub_ = create_subscription<pedsim_msgs::msg::AgentStates>(
       "pedsim_simulator/simulated_agents", rclcpp::SensorDataQoS(),
       std::bind(&PedsimRelayNode::agentsCallback, this, std::placeholders::_1));
   // Subscribe to local costmap topic
   costmap_sub_ = create_subscription<nav_msgs::msg::OccupancyGrid>(
-      local_costmap_topic, 10,
+      costmap_topic, 10,
       std::bind(&PedsimRelayNode::costmapCallback, this, std::placeholders::_1));
   // Publish filtered agents
   det_agents_pub_ = this->create_publisher<soloco_interfaces::msg::TrackedAgents>(detected_agents_topic, 10);
+}
+
+void PedsimRelayNode::odomCallback(
+    const nav_msgs::msg::Odometry::SharedPtr msg){
+  robot_pose_ = msg->pose.pose;
+  tf2::Quaternion q(
+    robot_pose_.orientation.x, 
+    robot_pose_.orientation.y, 
+    robot_pose_.orientation.z, 
+    robot_pose_.orientation.w);
+  // Convert quaternion to roll, pitch, yaw
+  double roll, pitch, yaw;
+  tf2::Matrix3x3(q).getRPY(roll, pitch, yaw);
+  robot_theta_ = yaw;
 }
 
 void PedsimRelayNode::agentsCallback(
@@ -47,8 +68,8 @@ void PedsimRelayNode::agentsCallback(
   for (const auto &actor : msg->agent_states) {
     double pos_x = actor.pose.position.x;
     double pos_y = actor.pose.position.y;
-    // Check if agent is insde local costmap
-    bool occ_flag = check_agent_in_local_costmap(pos_x, pos_y);
+    // Check if agent is insde local costmap and fov
+    bool occ_flag = check_agent_in_fov_and_costmap(pos_x, pos_y);
     if (occ_flag) {
       geometry_msgs::msg::PoseStamped ps_;
       soloco_interfaces::msg::TrackedAgent agent_;
@@ -81,14 +102,19 @@ void PedsimRelayNode::costmapCallback(
   map_resolution = costmap_msg->info.resolution;
 }
 
-bool PedsimRelayNode::check_agent_in_local_costmap(
+bool PedsimRelayNode::check_agent_in_fov_and_costmap(
     double agent_x, double agent_y) {
+  // Calculate angle to agent
+  double angle_to_agent = std::atan2(agent_y - robot_pose_.position.y, agent_x - robot_pose_.position.x);
+  angle_to_agent = angles::normalize_angle(angle_to_agent);
+  // Check if the agent is within the FOV
+  double angle_diff = angles::normalize_angle(angle_to_agent - robot_theta_);
+  bool agent_in_fov = std::abs(angle_diff) <= field_of_view_ / 2;
   int grid_x = (int)((agent_x - map_origin_x) / map_resolution);
   int grid_y = (int)((agent_y - map_origin_y) / map_resolution);
-  if (grid_x >= 0 && grid_x < map_width && grid_y >= 0 && grid_y < map_height) {
-    return true;
-  }
-  else { return false; }
+  bool in_costmap = grid_x >= 0 && grid_x < map_width && grid_y >= 0 && grid_y < map_height;
+
+  return agent_in_fov && in_costmap;
 }
 
 }; // namespace pedsim
